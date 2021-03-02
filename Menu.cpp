f2528bb3590c6e63dbc1e2adf5bf66b07c2131a7
roobtj11 <roobtj11@users.noreply.github.com>
2021-03-02 12:42:46 -0600

    Menu addition

---
+++ Menu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+void Startup_Script() {
+	std::cout << "               Welcome to the" << std::endl;
+	std::cout << "               SPORTS MACHINE" << std::endl;
+	std::cout << "     Made by: Tyler Roob and Jack Schnor" << std::endl;
+
+	std::cout << "This program will let you, Keep track of team rosters for - Volleyball, Basketball, and Soccer\n ";
+}
+
+int main() {
+	Startup_Script();
+
+
+	return 0;
+}
