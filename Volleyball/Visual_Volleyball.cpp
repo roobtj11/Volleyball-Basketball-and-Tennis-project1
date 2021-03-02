881366b76866d9b099af734250ad8c9fbc2c242d
roobtj11 <roobtj11@users.noreply.github.com>
2021-03-02 15:30:47 -0600

    Fixed

--- Volleyball/Visual_Volleyball.cpp
+++ Volleyball/Visual_Volleyball.cpp
@@ -7,12 +7,12 @@
 const int Num_Per_Roster = 12;
 const int Num_Per_Side = 6;
 
-void vball_visual(std::string Home_Lineup[][Num_Per_Side], std::string Away_Lineup[][Num_Per_Side]);
 void Get_Teams(std::string&, std::string&);
-void get_vball_rosters(std::string, std::string, std::string Volleyball_Home_Names[], std::string Volleyball_Home_Numbers[], std::string Volleyball_Home_Positions[], std::string Volleyball_Away_Names[],std::string Volleyball_away_Numbers[], std::string Volleyball_Away_Positions[]);
-void get_vball_rosters(std::string, std::string, std::string Home_Names[], std::string Volleyball_Home_Numbers[], std::string Volleyball_Home_Positions[], std::string Volleyball_Away_Names[], std::string Volleyball_away_Numbers[], std::string Volleyball_Away_Positions[]);
-void On_Court(std::string, std::string Names[], std::string Numbers[], std::string Positions[], std::string Lineup[][Num_Per_Side]);
-void Starting_Lineups(std::string, std::string, std::string Home_Names[], std::string Home_Numbers[], std::string Home_Positions[], std::string Away_Names[], std::string Away_Numbers[], std::string Away_Positions[], std::string Home_Lineup[Num_Per_Side][Num_Per_Side], std::string Away_Lineup[Num_Per_Side][Num_Per_Side]);
+void get_vball_rosters(std::string, std::string, std::string Volleyball_Home_Names[], std::string Volleyball_Home_Numbers[], std::string Volleyball_Home_Positions[], std::string Volleyball_Away_Names[], std::string Volleyball_away_Numbers[], std::string Volleyball_Away_Positions[]);
+void vball_visual(std::string Home_Lineup[], std::string Away_Lineup[]);
+void read_data_from_file(std::string filename, std::string Names[], std::string Numbers[], std::string Positions[]);
+void On_Court(std::string, std::string Names[], std::string Numbers[], std::string Positions[], std::string Lineup[]);
+void Starting_Lineups(std::string, std::string, std::string Home_Names[], std::string Home_Numbers[], std::string Home_Positions[], std::string Away_Names[], std::string Away_Numbers[], std::string Away_Positions[], std::string Home_Lineup[], std::string Away_Lineup[]);
 
 int main() {
 
@@ -24,18 +24,13 @@ int main() {
 	std::string Volleyball_Away_Names[Num_Per_Roster];
 	std::string Volleyball_away_Numbers[Num_Per_Roster];
 	std::string Volleyball_Away_Positions[Num_Per_Roster];
-	std::string Home_Lineup[Num_Per_Side][Num_Per_Side]{};
-	std::string Away_Lineup[Num_Per_Side][Num_Per_Side]{};
+	std::string Home_Lineup[Num_Per_Side];
+	std::string Away_Lineup[Num_Per_Side];
 
 	Get_Teams(home, away);
 	get_vball_rosters(home, away, Volleyball_Home_Names, Volleyball_Home_Numbers, Volleyball_Home_Positions, Volleyball_Away_Names, Volleyball_away_Numbers, Volleyball_Away_Positions);
-	
 	Starting_Lineups(home, away, Volleyball_Home_Names, Volleyball_Home_Numbers, Volleyball_Home_Positions, Volleyball_Away_Names, Volleyball_away_Numbers, Volleyball_Away_Positions, Home_Lineup, Away_Lineup);
-	
-	vball_visual(Home_Lineup, Away_Lineup);
-
-
-	
+	vball_visual(Home_Lineup, Away_Lineup);	
 
 	return 0;
 }
@@ -47,20 +42,19 @@ void Get_Teams(std::string& home, std::string& away) {
 	std::getline(std::cin, away);
 }
 
-void vball_visual(std::string Home_Lineup[][Num_Per_Side], std::string Away_Lineup[][Num_Per_Side]) {
+void vball_visual(std::string Home_Lineup[], std::string Away_Lineup[]) {
 
 	std::cout << " ____________________ " << std::endl;
 	std::cout << "|      |      |      |" << std::endl;
-	std::cout << "|  " << Home_Lineup[0][0] << "  |  " << Home_Lineup[0][5] << "  |  " << Home_Lineup[0][4] << "  |" << std::endl;
+	std::cout << "|  " << Home_Lineup[0] << "  |  " << Home_Lineup[5] << "  |  " << Home_Lineup[4] << "  |" << std::endl;
 	std::cout << "|------|------|------|" << std::endl;
-	std::cout << "|  " << Home_Lineup[0][1] << "  |  " << Home_Lineup[0][2] << "  |  " << Home_Lineup[0][3] << "  |" << std::endl;
+	std::cout << "|  " << Home_Lineup[1] << "  |  " << Home_Lineup[2] << "  |  " << Home_Lineup[3] << "  |" << std::endl;
 	std::cout << "======================" << std::endl;
-	std::cout << "|  " << Away_Lineup[0][3] << "  |  " << Away_Lineup[0][2] << "  |  " << Away_Lineup[0][1] << "  |" << std::endl;
+	std::cout << "|  " << Away_Lineup[3] << "  |  " << Away_Lineup[2] << "  |  " << Away_Lineup[1] << "  |" << std::endl;
 	std::cout << "|------|------|------|" << std::endl;
-	std::cout << "|  " << Away_Lineup[0][4] << "  |  " << Away_Lineup[0][5] << "  |  " << Away_Lineup[0][0] << "  |" << std::endl;
+	std::cout << "|  " << Away_Lineup[4] << "  |  " << Away_Lineup[5] << "  |  " << Away_Lineup[0] << "  |" << std::endl;
 	std::cout << "|______|______|______|" << std::endl;
 }
-
 
 void get_vball_rosters(std::string home, std::string away,std::string Home_Names[], std::string Volleyball_Home_Numbers[], std::string Volleyball_Home_Positions[], std::string Volleyball_Away_Names[], std::string Volleyball_away_Numbers[], std::string Volleyball_Away_Positions[]) {
 	std::cout << "Reading home team roster... " << std::endl;
@@ -127,7 +121,7 @@ void read_data_from_file(std::string filename, std::string Names[], std::string 
 	stream.close();
 }
 
-void On_Court(std::string team, std::string Names[], std::string Numbers[], std::string Positions[], std::string Lineup[][Num_Per_Side]) {
+void On_Court(std::string team, std::string Names[], std::string Numbers[], std::string Positions[], std::string Lineup[]) {
 	std::cout << "Here is the roster for " << team << ":" << std::endl << std::endl;
 	for (int i = 0; i < Num_Per_Roster; i++) {
 		std::cout << Names[i] << "\t" << Numbers[i] << "\t" << Positions[i] << std::endl;
@@ -140,13 +134,13 @@ void On_Court(std::string team, std::string Names[], std::string Numbers[], std:
 		std::getline(std::cin, number);
 		for (int p = 0; p < Num_Per_Roster; p++) {
 			if (Numbers[p] == number) {
-				Lineup[0][p] = Numbers[p];
+				Lineup[i] = Numbers[p];
 			}
 		}
 	}
 }
 
-void Starting_Lineups(std::string home, std::string away, std::string Home_Names[], std::string Home_Numbers[], std::string Home_Positions[], std::string Away_Names[], std::string Away_Numbers[], std::string Away_Positions[], std::string Home_Lineup[Num_Per_Side][Num_Per_Side], std::string Away_Lineup[Num_Per_Side][Num_Per_Side]) {
+void Starting_Lineups(std::string home, std::string away, std::string Home_Names[], std::string Home_Numbers[], std::string Home_Positions[], std::string Away_Names[], std::string Away_Numbers[], std::string Away_Positions[], std::string Home_Lineup[], std::string Away_Lineup[]) {
 	try {
 		On_Court(home, Home_Names, Home_Numbers, Home_Positions, Home_Lineup);
 		std::cout << "Success!" << std::endl;
